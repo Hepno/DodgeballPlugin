f4131214673c79be936dd2ffaa9985652a53cc51
hepno <me@hepno.dev>
2023-03-18 22:38:27 +0100

    init

---
+++ src/main/java/dev/hepno/dodgeball/Dodgeball.java
@@ -0,0 +1,16 @@
+package dev.hepno.dodgeball;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Dodgeball extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
