a0fe0ddb0c7e0836d1f3e3dd4c57445358b58dcb
hepno <me@hepno.dev>
2023-03-20 20:58:21 +0100

    fix(ArenaCommand): improve command by inrceasing code readability and adding permissions

--- src/main/java/dev/hepno/dodgeball/Commands/ArenaCommand.java
+++ src/main/java/dev/hepno/dodgeball/Commands/ArenaCommand.java
@@ -20,16 +20,116 @@ public class ArenaCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) { return false; }
+        Player player = (Player) sender;
+        if (!(player.hasPermission("dodgeball.arena"))) {
+            player.sendMessage(ChatColor.RED + "You do not have permission to use this command!");
+            return false;
+        }
+
+        if (args.length == 0) {
+            player.sendMessage(ChatColor.RED + "Usage: /arena <list|join|leave|teams>");
+            return false;
+        }
+
+        if (args[0].equalsIgnoreCase("list")) {
+            if (!(player.hasPermission("dodgeball.arena.list"))) {
+                player.sendMessage(ChatColor.RED + "You do not have permission to use this command!");
+                return false;
+            }
+            player.sendMessage(ChatColor.GREEN + "List of arenas:");
+            for (Arena arena : plugin.getArenaManager().getArenas()) {
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&a- &7Arena " + arena.getId() + " &a(&7" + arena.getState().toString() + "&a)"));
+            }
+            return true;
+        }
+
+        if (args[0].equalsIgnoreCase("join")) {
+            if (!(player.hasPermission("dodgeball.arena.join"))) {
+                player.sendMessage(ChatColor.RED + "You do not have permission to use this command!");
+                return false;
+            }
+            if (args.length < 2) {
+                player.sendMessage(ChatColor.RED + "Usage: /arena join <id>");
+                return false;
+            }
+            Arena arena = plugin.getArenaManager().getArena(Integer.parseInt(args[1]));
+            if (arena == null) {
+                player.sendMessage(ChatColor.RED + "Arena " + args[1] + " does not exist!");
+                return false;
+            }
+            if (arena.getState() == GameState.LIVE) {
+                player.sendMessage(ChatColor.RED + "Arena " + args[1] + " is already in progress!");
+                return false;
+            }
+            arena.addPlayer(player);
+            return true;
+        }
+
+        if (args[0].equalsIgnoreCase("teams")) {
+            if (!(player.hasPermission("dodgeball.arena.teams"))) {
+                player.sendMessage(ChatColor.RED + "You do not have permission to use this command!");
+                return false;
+            }
+            Arena arena = plugin.getArenaManager().getArena(player);
+            if (arena != null) {
+                if (arena.getState() != GameState.LIVE) {
+                    if (arena.getTeamCount(arena.getTeam(player)) == 1) {
+                        player.sendMessage(ChatColor.RED + "You can't change teams while you are the only one in your team!");
+                        return false;
+                    }
+                    new TeamGUI(arena, player);
+                } else {
+                    player.sendMessage(ChatColor.RED + "You can't change teams while the game is in progress!");
+                }
+            } else {
+                player.sendMessage(ChatColor.RED + "You are not in an arena!");
+            }
+            return true;
+        }
+
+        if (args[0].equalsIgnoreCase("leave")) {
+            if (!(player.hasPermission("dodgeball.arena.leave"))) {
+                player.sendMessage(ChatColor.RED + "You do not have permission to use this command!");
+                return false;
+            }
+            Arena arena = plugin.getArenaManager().getArena(player);
+            if (arena != null) {
+                arena.removePlayer(player);
+            } else {
+                player.sendMessage(ChatColor.RED + "You are not in an arena!");
+            }
+            return true;
+        }
+
+
+
+        return false;
+    }
+
+    /* @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
         if (sender instanceof Player) {
             Player player = (Player) sender;
 
+            if (!(player.hasPermission("dodgeball.arena"))) {
+                player.sendMessage(ChatColor.RED + "You do not have permission to use this command!"); return false;
+            }
+
             if (args.length == 1 && args[0].equalsIgnoreCase("list")) {
+                if (!(player.hasPermission("dodgeball.arena.list"))) {
+                    player.sendMessage(ChatColor.RED + "You do not have permission to use this command!"); return false;
+                }
                 player.sendMessage(ChatColor.GREEN + "List of arenas:");
                 for (Arena arena : plugin.getArenaManager().getArenas()) {
                     player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&a- &7Arena " + arena.getId() + " &a(&7" + arena.getState().name() + "&a)"));
                 }
             } else if (args.length == 1 && args[0].equalsIgnoreCase("teams")) {
+                if (!(player.hasPermission("dodgeball.arena.teams"))) {
+                    player.sendMessage(ChatColor.RED + "You do not have permission to use this command!"); return false;
+                }
+
                 Arena arena = plugin.getArenaManager().getArena(player);
                 if (arena != null) {
                     if (arena.getState() != GameState.LIVE) {
@@ -45,6 +145,10 @@ public class ArenaCommand implements CommandExecutor {
                 } else {
                     player.sendMessage(ChatColor.RED + "You are not in an arena!");
             } if (args.length == 1 && args[0].equalsIgnoreCase("leave")) {
+                if (!(player.hasPermission("dodgeball.arena.leave"))) {
+                    player.sendMessage(ChatColor.RED + "You do not have permission to use this command!"); return false;
+                }
+
                 Arena arena = plugin.getArenaManager().getArena(player);
                 if (arena != null) {
                     arena.removePlayer(player);
@@ -53,7 +157,11 @@ public class ArenaCommand implements CommandExecutor {
                     player.sendMessage(ChatColor.RED + "You are not in an arena!");
                 }
             } else if (args.length == 2 && args[0].equalsIgnoreCase("join")) {
-                if (plugin.getArenaManager().getArena(player) != null) {
+                if (!(player.hasPermission("dodgeball.arena.join"))) {
+                    player.sendMessage(ChatColor.RED + "You do not have permission to use this command!"); return false;
+                }
+
+                if (plugin.getArenaManager().getArena(player) != null && args.length == 2) {
                     player.sendMessage(ChatColor.RED + "You are already in an arena!");
                     return false;
                 }
@@ -87,5 +195,5 @@ public class ArenaCommand implements CommandExecutor {
         }
 
         return false;
-    }
+    } */
 }
